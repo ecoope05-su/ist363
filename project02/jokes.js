3991619a627dd228beb0bb219fc9b2de16789d95
ecoope05-su <ecoope05@syr.edu>
2025-04-04 15:53:54 -0400

    proj 2

--- project02/jokes.js
+++ project02/jokes.js
@@ -1,3 +1,5 @@
+//jokes
+
 const getJoke = async () => {
 
     const response = await fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart');
@@ -32,6 +34,75 @@ function refreshJoke() {
         document.getElementById('jokesetup').innerHTML =  data.setup;
         document.getElementById('jokepunchline').innerHTML =  data.delivery;
         document.getElementById('jokepunchline').style.display = 'none';
+    })
+    .catch(err => console.log('rejected: ', err.message));
+}
+
+//cats
+
+const getCat = async () => {
+
+    const response = await fetch('https://cataas.com/cat?json=true');
+
+    if(response.status !== 200){
+        throw new Error('Cannot fetch data');
+    }
+
+    const catData = await response.json();
+
+    return catData;
+
+};
+
+refreshCats();
+
+document.getElementById('refreshcats').addEventListener('click', refreshCats);
+
+function refreshCats(){
+    loadCat('cat1');
+    loadCat('cat2');
+    loadCat('cat3');
+    loadCat('cat4');
+    loadCat('cat5');
+    loadCat('cat6');
+
+};
+
+function loadCat(id) {
+    getCat()
+    .then(catData => {
+        console.log('resolved: ', catData)
+        document.getElementById(id).src = catData.url;
+        
+    })
+    .catch(err => console.log('rejected: ', err.message));
+};
+
+//fun facts
+
+const getFacts = async () => {
+
+    const response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en');
+
+    if(response.status !== 200){
+        throw new Error('Cannot fetch data');
+    }
+
+    const factData = await response.json();
+
+    return factData;
+
+};
+
+loadFact();
+
+document.getElementById('newfact').addEventListener('click', loadFact);
+
+function loadFact() {
+    getFacts()
+    .then(factData => {
+        console.log('resolved: ', factData)
+        document.getElementById('funfact').innerHTML = factData.text;
     })
     .catch(err => console.log('rejected: ', err.message));
 }
